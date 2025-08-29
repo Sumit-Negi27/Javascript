cb940dc17e14617833ccf1aae9795004a2db2ff2
sumit negi <negi759123@gmail.com>
2025-08-29 23:10:48 +0530

    edit

---
+++ 5.string/index.js
@@ -0,0 +1,15 @@
+let a="SUMIT"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log(a.length)
+console.log("HEY MY NAME IS :"+a + " NEGI")
+console.log('HEY MY NAME IS :',a, 'NEGI')
+
+console.log(a.toLowerCase())
+console.log(a.slice(1,4))
+console.log(a.concat(" negi"," is good boy"))
+console.log(a.replace("S","X"))
