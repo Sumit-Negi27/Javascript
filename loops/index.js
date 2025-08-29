8e9ca6bff6ac9f6700f4ab7c63e3d0118dcc8d20
sumit negi <negi759123@gmail.com>
2025-08-29 13:04:04 +0530

    added new file

---
+++ loops/index.js
@@ -0,0 +1,25 @@
+console.log("loops")
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+
+}
+// basic for loop
+let o = {
+    name: "sumit", age: 18,marks: 100
+
+}
+
+for (const key in o) {
+    const element = o[key];
+    console.log(element)
+    console.log(key)
+}
+
+for (const a of "sumit") {
+    console.log(a)
+}
+let a=1
+while (a<10) {
+    console.log(a)
+    a++
+}
