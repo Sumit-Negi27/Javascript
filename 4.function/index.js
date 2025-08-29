119f780702a1cf78ec0a7df320f42f57992f9b67
sumit negi <negi759123@gmail.com>
2025-08-29 21:06:00 +0530

    add new folder

---
+++ 4.function/index.js
@@ -0,0 +1,12 @@
+console.log("hey this is function program")
+function sum(a,b) {
+    return a+b 
+}
+a=sum(2,4)
+console.log(a)
+
+const fun1=(x)=>
+{
+    console.log("i am arrow function",x)
+}
+fun1(100)
