d7e3b0c6d65745a5106a1b47483eadc719b16465
sumit negi <negi759123@gmail.com>
2025-08-29 10:22:19 +0530

    added new file

---
+++ 1.variables_datatype_object/console.js
@@ -0,0 +1,10 @@
+console.log("hey this is sumit negi")
+
+var a=5;
+var b=1000;
+var c="sumit"
+console.log(typeof a, typeof b, typeof c)
+let o={ "name": "sumit",salary:1000000000, age:19}
+console.log(o)
+o.handsome=true
+console.log(o)
